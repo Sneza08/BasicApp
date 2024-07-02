39187fa2f8db6cf487c93b7fe5cae1598ec0c217
SnezanaBogdanovic <snezanabogdanovic79@gmail.com>
2024-07-02 17:04:11 +0200

    Dodavanje objekta i kretanje

--- Primer1/Beach.java
+++ Primer1/Beach.java
@@ -16,6 +16,9 @@ public class Beach extends World
     public Beach()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(200, 150, 5); 
+        super(15 ,10, 60); 
+        this.addObject(new Police(), 0,0);
+        
+        
     }
 }
