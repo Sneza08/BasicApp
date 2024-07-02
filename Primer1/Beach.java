85e4935b955de37d38a0f4e6f724c1c0d8c9ced3
SnezanaBogdanovic <snezanabogdanovic79@gmail.com>
2024-07-02 16:30:39 +0200

    Promena pozadine2

--- Primer1/Beach.java
+++ Primer1/Beach.java
@@ -16,6 +16,6 @@ public class Beach extends World
     public Beach()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(200, 150, 5); 
     }
 }
